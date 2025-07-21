1753498c3bca01975fbce8f2e8258afe9f642679
Ayan Paul <ayancse2001@gmail.com>
2025-07-21 15:15:34 +0600

    Initial Run Successful!

--- Admin/admin.controller.ts
+++ Admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { AdminService } from './admin.service';
 
 @Controller('admin')
--- app.module.ts
+++ app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminController } from './Admin/admin.controller';
-
+import { AdminService } from './Admin/admin.service';
 @Module({
   imports: [],
-  controllers: [AdminController, AdminController],
-  providers: [AppService],
+  controllers: [AppController, AdminController],
+  providers: [AppService, AdminService],
 })
 export class AppModule {}
